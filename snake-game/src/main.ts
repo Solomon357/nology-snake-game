6e14e856bd49afb33316570a3ba28d902a67b793
Solomon357 <Solomonoddy@hotmail.com>
2024-08-28 17:07:20 +0100

    continous movement with one arrow key press integrated, introduced a game-area bug though :(

--- snake-game/src/main.ts
+++ snake-game/src/main.ts
@@ -6,32 +6,68 @@ const player = document.querySelector<HTMLDivElement>('#player')!;
 //getting CSS styles 
 const playerStyleTop = getComputedStyle(player).getPropertyValue('top')
 const playerStyleLeft = getComputedStyle(player).getPropertyValue('left')
-//output test
+
+//visual output test
 let currentXPosOutput = document.querySelector<HTMLParagraphElement>('#playerX')!;
 let currentYPosOutput = document.querySelector<HTMLParagraphElement>('#playerY')!;
 currentYPosOutput.innerHTML = `current Y position: ${playerStyleTop}`;
 currentXPosOutput.innerHTML = `current X position: ${playerStyleLeft}`;
 
+//Interval ID to make sure only one interval is running at a time
+let intervalId: number;
 
+// TODO: 1. need to fix the container issue that I didnt have before 
+//        - WILL PROBABLY NEED ANOTHER FUNCTION TO HANDLE any GAMEOVER condition
+//       2. will need to incorporate food pellets and a score system
+// const gameOver = () => {
 
-const moveUp = (YPosition: string | number): number => {
-  YPosition = +YPosition - 20;
+// }
+
+// all directions need to check for both X and Y positions so no 
+const handleMoveUp = (XPosition: string | number, YPosition: string | number) => {
+  
+  if(+YPosition >= 0 && +YPosition <= 380 && +XPosition >= 0 && +XPosition <= 680){
+    YPosition = +YPosition - 20;
+    player.style.top = `${YPosition}px`;
+    // console.log(player.style.top)
+  } else{
+    player.style.top = `${YPosition}px`;
+  }
   return YPosition;
 }
-const moveDown = (YPosition: string | number): number => {
-  YPosition = +YPosition + 20;
+const handleMoveDown = (XPosition:string | number, YPosition: string | number) => {
+  if(+YPosition >= 0 && +YPosition <= 380 && +XPosition >= 0 && +XPosition <= 680){
+    YPosition = +YPosition + 20;
+    player.style.top = `${YPosition}px`;
+    // console.log(player.style.top)
+  } else{
+    player.style.top = `${YPosition}px`;
+  }
   return YPosition;
 }
-const moveLeft = (XPosition: string | number): number => {
-  XPosition = +XPosition - 20;
+const handleMoveLeft = (XPosition: string | number, YPosition: string | number) => {
+  if(+XPosition >= 0 && +XPosition <= 680 && +YPosition >= 0 && +YPosition <= 380){
+    XPosition = +XPosition - 20;
+    player.style.left = `${XPosition}px`;
+  } else {
+    player.style.top = `${XPosition}px`;
+  }
   return XPosition;
 }
-const moveRight = (XPosition: string | number): number => {
-  XPosition = +XPosition + 20;
+const handleMoveRight = (XPosition: string | number, YPosition: string | number) => {
+  if(+XPosition >= 0 && +XPosition <= 680 && +YPosition >= 0 && +YPosition <= 380){
+    XPosition = +XPosition + 20;
+    player.style.left = `${XPosition}px`;
+    // console.log(player.style.top)
+  } else{
+    player.style.top = `${XPosition}px`;
+  }
   return XPosition;
 }
+
 const handlePlayerMovement = (e: KeyboardEvent) => {
   e.preventDefault(); // to prevent arrows scrolling the window up
+  e.stopPropagation(); //not sure if i need this just yet
 
   // "player.style.${attr}" is of type "CSSInlineStyle" so in order for this to work how I expect I need the top and left attributes of player to be inline styles beforehand
   let currentYPos: string | number = player.style.top;
@@ -40,65 +76,51 @@ const handlePlayerMovement = (e: KeyboardEvent) => {
   currentYPos = currentYPos.replace("px", ""); // e.g. currentYPos = "20"
   currentXPos = currentXPos.replace("px", ""); // e.g. currentXPos = "0"
 
-  //1. TRY LOOPING ANOTHER FUNCTION WITH EVENT INSIDE
   switch(e.key){
     case "ArrowUp":
-      //need to check for both X and Y positions for all directions 
-      if(+currentYPos >= 0 && +currentYPos <= 380 && +currentXPos >= 0 && +currentXPos <= 680){
-        currentYPos = moveUp(currentYPos);
-        player.style.top = `${currentYPos}px`;
-        // console.log(player.style.top)
-      } else{
-        currentYPos = +currentYPos - 0;
-        player.style.top = `${currentYPos}px`;
-      }
-      // show change to the user
-      currentYPosOutput.innerHTML = `current Y position: ${player.style.top}`;
+      // making sure the last interval is cleared before setting a new one 
+      clearInterval(intervalId);
+
+      intervalId = setInterval(() => {
+        currentYPos = handleMoveUp(currentXPos, currentYPos);
+        // show change to the user
+        currentYPosOutput.innerHTML = `current Y position: ${player.style.top}`;
+      }, 100);
       break;
 
     case "ArrowDown":
-      if(+currentYPos >= 0 && +currentYPos <= 380 && +currentXPos >= 0 && +currentXPos <= 680){
-        currentYPos = moveDown(currentYPos);
-        player.style.top = `${currentYPos}px`;
-        // console.log(player.style.top);
-        
-      } else{
-        currentYPos = +currentYPos + 0;
-        player.style.top = `${currentYPos}px`;
-      }
-      // show change to the user regardless of conditional
-      currentYPosOutput.innerHTML = `current Y position: ${player.style.top}`;
+      clearInterval(intervalId);
+
+      intervalId = setInterval(() => {
+        currentYPos = handleMoveDown(currentXPos, currentYPos);
+        // show change to the user regardless of conditional
+        currentYPosOutput.innerHTML = `current Y position: ${player.style.top}`;
+      }, 100);
       break; 
 
     case "ArrowLeft":
-      if(+currentXPos >= 0 && +currentXPos <= 680 && +currentYPos >= 0 && +currentYPos <= 380){
-        currentXPos = moveLeft(currentXPos)
-        player.style.left = `${currentXPos}px`;
-        // console.log(player.style.left)
-      } else {
-        currentXPos = +currentXPos - 0;
-        player.style.left = `${currentXPos}px`;
-      }
-      // show change to the user
-      currentXPosOutput.innerHTML = `current X position: ${player.style.left}`;
+      clearInterval(intervalId);
+      intervalId = setInterval(() => {
+        currentXPos = handleMoveLeft(currentXPos, currentYPos);
+         //show change to the user
+        currentXPosOutput.innerHTML = `current X position: ${player.style.left}`;
+      }, 100);
       break; 
 
     case "ArrowRight":
-      if(+currentXPos >= 0 && +currentXPos <= 680 && +currentYPos >= 0 && +currentYPos <= 380){
-        currentXPos = moveRight(currentXPos) 
-        player.style.left = `${currentXPos}px`;
-        // console.log(player.style.left)
-      } else {
-        currentXPos = +currentXPos + 0;
-        player.style.left = `${currentXPos}px`;
-      }
-      // show change to the user
-      currentXPosOutput.innerHTML = `current X position: ${player.style.left}`;
+
+      clearInterval(intervalId);
+      intervalId = setInterval(() => {
+        currentXPos = handleMoveRight(currentXPos, currentYPos);
+        // show change to the user
+        currentXPosOutput.innerHTML = `current X position: ${player.style.left}`;
+      }, 100);
       break;
 
     default:
       break;
   }
 }
+
 
 document.addEventListener("keydown", handlePlayerMovement);
